b3b1bd9100a525a383b3c049197d67465891115e
Sam <hol_sam@icloud.com>
2026-03-22 15:24:21 +0000

    fix(ev-post): fix morphology closure footprint
    Updated ev-post-processing.py to use morphology.ball() to generate
    footprint for morphological closure instead of morphology.disk(). This
    now generates a 3D footprint so should enable applying closure across
    the entire segmentation mask instead of throwing an error as it tuple
    indices did not match. Also updated process_segmentation() to attempt
    morphological closure but if error thrown, show warning and continue
    with non-closed data.

--- ev-post-processing.py
+++ ev-post-processing.py
@@ -136,6 +136,7 @@ def morphological_closure(binary_vol: numpy.ndarray):
     '''
     Applies a morphological closing operation (dilation followed by erosion). This removes small dark spots and connects small bright cracks (i.e. helps fill in EV membranes which may not be segmented perfectly).
     '''
+<<<<<<< HEAD
     binary_vol_closed=ndimage.binary_closing(binary_vol)
     return binary_vol_closed
 
@@ -147,6 +148,9 @@ def morphological_closure(binary_vol: numpy.ndarray):
     Applies a morphological closing operation (dilation followed by erosion). This removes small dark spots and connects small bright cracks (i.e. helps fill in EV membranes which may not be segmented perfectly).
     '''
     footprint=morphology.disk(6)
+=======
+    footprint=morphology.ball(6)
+>>>>>>> 174896d (fix(ev-post): fix morphology closure footprint)
     binary_vol_closed=morphology.closing(binary_vol, footprint)
     return binary_vol_closed
 
@@ -407,8 +411,11 @@ def process_segmentation(seg_path: Path):
         lg.debug(f"{seg_path.name} - applied morphological closure.")
     except:
         lg.warning(f"{seg_path.name} - error applying morphological closure.")
+<<<<<<< HEAD
     # Apply morphological closure to segmentation mask
     data = morphological_closure(data)
+=======
+>>>>>>> 174896d (fix(ev-post): fix morphology closure footprint)
     # Label components and get number of components
     components, n_components = label_components(data)
     lg.debug(f"{seg_path.name} - labelled components.")
