7cc0048ccd4d922f74c2cff422139a647034ca2b
Sam Holland <hol_sam@icloud.com>
2026-04-13 19:19:30 +0100

    fix(license): fix license packaging
    - Modified license.py to fix packaging.
    - Modified pyproject.toml to include LICENSE with built package.
    - Modified pyproject.toml and uv.lock to reflect version.
    
    Version number bumped to: 0.6.6

--- src/evaluator/commands/license.py
+++ src/evaluator/commands/license.py
@@ -7,12 +7,13 @@ EValuator: PRINT LICENSE
 # Import external dependencies
 # ====================
 import typer
+from importlib.resources import files as pkg_files
 from rich import print
 
 # ====================
 # Define command: license
 # ====================
 def printLicense():
-    with open('./LICENSE', 'r') as licensefile:
-        print(f"\n{licensefile.read()}")
+    with pkg_files('evaluator').joinpath('LICENSE').open('r') as f:
+        print(f.read())
     typer.Exit(0)
